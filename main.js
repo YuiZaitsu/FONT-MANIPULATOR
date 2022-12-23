5d11c2c77d867a78a9f90ccfcc674a05239ec953
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2022-12-23 10:03:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftwristx=0;
+rightwristx=0;
+difference=0;
 function preload(){
 
 }
@@ -12,7 +15,12 @@ posenet.on("pose",gotposes);
 }
 
 function draw(){
-background("black");
+background("violet");
+textSize(difference);
+text('Karthikeyan', 10, 200);
+fill(0, 102, 153);
+document.getElementById("text_sides").innerHTML=" The text size will be = "+difference+"px";
+
 }
 
 function modelloaded(){
@@ -21,8 +29,18 @@ function modelloaded(){
     
 }
 
-function gotposes(reuslts){
-if(reuslts.length > 0){
-    console.log(reuslts);
-}
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
 }
